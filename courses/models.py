b9e44d62d85e924da47e25ae8226758704c50d52
Benson <bensonpx1198@gmail.com>
2020-02-26 13:10:51 +0530

    third-commit

--- courses/models.py
+++ courses/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 
+from django.urls import reverse
 from memberships.models import Membership
 
 class Course(models.Model):
-    slug = model.SlugField()
-    title = models.CharField(max_field = 120)
+    slug = models.SlugField()
+    title = models.CharField(max_length = 120)
     description = models.TextField()
     allowed_memberships = models.ManyToManyField(Membership)
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('courses:detail' , kwargs = {'slug': self.slug})
 class Lesson(models.Model):
     slug = models.SlugField()
-    title = modles.CharField(max_length = 120)
+    title = models.CharField(max_length = 120)
     course = models.ForeignKey(Course, on_delete=models.SET_NULL, null = True)
     position = models.IntegerField()
     video_url = models.CharField(max_length=200)
