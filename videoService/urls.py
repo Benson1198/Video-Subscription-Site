63a693de3aa09d99b352436abd1f4a7f4261ff33
Benson <bensonpx1198@gmail.com>
2020-03-16 00:45:55 +0530

    commit-5

--- videoService/urls.py
+++ videoService/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('courses.urls'),name = 'courses'),
+    path('',include('courses.urls',namespace = "courses")),
 ]
 
 if settings.DEBUG:
